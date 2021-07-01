aa35e57caf369a6d7d9ef2963fe95136ace5136f
kalpesh12341 <triplehhh004@gmail.com>
2021-07-02 02:03:41 +0530

    Added Courses module

--- routes/courses.js
+++ routes/courses.js
@@ -2,11 +2,18 @@
 const express = require('express')
 
 // importing logic of all routes i.e controllers by destructuring
-const { getCourses } = require('../controllers/courses')
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../controllers/courses')
 
 // Loader Express Router
 const router = express.Router({ mergeParams: true })
 
-router.route('/').get(getCourses)
+router.route('/').get(getCourses).post(addCourse)
 
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
 module.exports = router
